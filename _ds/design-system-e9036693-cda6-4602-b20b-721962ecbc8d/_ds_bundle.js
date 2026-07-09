919f4d4b3987685f25a295fdb91ed609973a8cab
Eduardo Araujo <edujunior254@gmail.com>
2026-07-09 16:00:03 -0300

    LP Artta - Calculadora (landing page)

---
+++ _ds/design-system-e9036693-cda6-4602-b20b-721962ecbc8d/_ds_bundle.js
@@ -0,0 +1,11 @@
+/* @ds-bundle: {"format":3,"namespace":"DesignSystem_e90366","components":[],"sourceHashes":{},"inlinedExternals":[],"unexposedExports":[]} */
+
+(() => {
+
+const __ds_ns = (window.DesignSystem_e90366 = window.DesignSystem_e90366 || {});
+
+const __ds_scope = {};
+
+(__ds_ns.__errors = __ds_ns.__errors || []);
+
+})();
